3739c270f456dcec28a6d5e782f36e6cca6b4462
Gergely Molnar <molnar.gergo18@gmail.com>
2020-10-05 12:55:17 +0200

    Initial commit

---
+++ src/main/java/com/codecool/fas/FakeAirlineServicesApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.fas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeAirlineServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeAirlineServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/fas/FakeAirlineServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.fas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeAirlineServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
