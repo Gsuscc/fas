88933cf055bdcb921de68f2be2cd09bb463f30e1
Tharegon <dombovari11david@gmaa>
2020-10-19 14:34:40 +0200

    city entity

---
+++ src/main/java/com/codecool/fas/entity/City.java
@@ -0,0 +1,31 @@
+package com.codecool.fas.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.net.URL;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class City {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(nullable = false)
+    private String cityName;
+    @Column(nullable = false)
+    private URL cityImage;
+    @Column(nullable = false)
+    private String countryName;
+    @Column(nullable = false,unique = true)
+    private String countryCode;
+    @OneToMany(mappedBy = "city", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @EqualsAndHashCode.Exclude
+    @Singular()
+    private Set<Airport> airport;
+
+}
